9157f015dd707433928a8fa23adc2a541ee55abe
Jorge O'Neill <jorge.oneill@wit-software.com>
2023-10-21 16:01:31 +0100

    Initial Commit

---
+++ StockMarketChallenge/StockMarketChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockMarketChallengeApp.swift
+//  StockMarketChallenge
+//
+//  Created by Jorge O'Neill on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct StockMarketChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
